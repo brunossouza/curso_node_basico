c8f0b306f0e8414aacd9da3768dfb98e8f5c8778
Bruno Silva <brunos.souza2@gmail.com>
2020-07-25 12:19:23 -0300

    update verificacao impar par

--- src/controller/exercicios.controller.js
+++ src/controller/exercicios.controller.js
@@ -3,7 +3,7 @@ module.exports = {
     async listOddPair(request, response) {
         let numeros = []
         for (let i = 1; i < 101; i++) {
-            numeros.push({ "numero": i, "ParOuImpar": (i % 2 == 0) ? "Par" : "Impar" })
+            numeros.push({ "numero": i, "ParOuImpar": (i % 2) ? "Impar" : "Par" })
         }
         let resposta = {
             "exercicio": "Crie um programa que imprima uma lista de 1 a 100, dizendo se o numero é par ou impar.",
